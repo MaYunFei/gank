33b742fd4f26a852fda564b4374e7cb503ea29c8
MaYunFei <mayunfei6@gmail.com>
2017-07-17 18:46:03 +0800

    添加依赖库

--- app/src/main/java/io/github/mayunfei/gank/MainActivity.java
+++ app/src/main/java/io/github/mayunfei/gank/MainActivity.java
@@ -1,12 +1,27 @@
 package io.github.mayunfei.gank;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
+    @BindView(R.id.viewpager)
+    ViewPager viewpager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
 }
